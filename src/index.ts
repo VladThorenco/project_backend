89aeb82f73ea0faa9e02f07ad9fc1fa80a5a8b92
Vlad Tkhorenko <vlad.thorenco18@gmail.com>
2023-08-31 15:19:52 +0300

    Init project with ts and nodemon

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,9 @@
-import express, { Request, Response } from "express";
-import dotenv from 'dotenv';
-dotenv.config()
-
+import express from 'express';
 const app = express()
-const port = process.env.PORT;
+const port = 3000
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("express")
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 })
 
 app.listen(port, () => {
