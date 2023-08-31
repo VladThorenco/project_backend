21528e549187a60cadbcf7468cd296a84db4b0b2
Vlad Tkhorenko <vlad.thorenco18@gmail.com>
2023-08-31 11:36:33 +0300

    Change port to env is file and added was it code

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from "express";
+import dotenv from 'dotenv';
+dotenv.config()
 
 const app = express()
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("express")
