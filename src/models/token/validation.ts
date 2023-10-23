62a11ce6af114c36405e7ed76730950fb534c807
Vlad Tkhorenko <vlad.thorenco18@gmail.com>
2023-10-23 17:50:32 +0300

    add types to model

--- src/models/token/validation.ts
+++ src/models/token/validation.ts
@@ -3,6 +3,6 @@ import { checkSchema } from "express-validator";
 export const refreshTokenValidation = checkSchema({
   refreshToken: {
     isString: true,
-    errorMessage: "Refresh Token is not valid",
+    errorMessage: "Refresh token should string",
   },
 });
